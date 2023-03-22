be4f93d5334b85d0a82551cd0450651c04bad340
Jigar Gajjar <jigargajjar.55jg@gmail.com>
2023-03-22 22:33:11 +0530

    10th Commit

---
+++ Dynamic Programming/BestTimeToBuyAndSellStock.java
@@ -0,0 +1,20 @@
+public class BestTimeToBuyAndSellStock {
+
+    // Time: O(N), Space: O(1)
+    public int maxProfit(int[] prices) {
+
+        int maxProfit = 0;
+        int mini = prices[0];
+
+        for (int i = 1; i < prices.length; i++) {
+            int cost = prices[i] - mini;
+
+            maxProfit = Math.max(maxProfit, cost);
+
+            mini = Math.min(mini, prices[i]);
+        }
+
+        return maxProfit;
+
+    }
+}
