379c8630cd3a495d89aea4b77a099abbf5a63d25
Jigar Gajjar <jigargajjar.55jg@gmail.com>
2023-04-22 17:22:58 +0530

    16th commit

--- Queue/DoublyEndedQueueMain.java
+++ Queue/DoublyEndedQueueMain.java
@@ -149,6 +149,18 @@ public class DoublyEndedQueueMain {
          * System.out.println(adq);
          * System.out.println(adq.pollLast());
          * System.out.println(adq.pollLast());
+         * System.out.println(adq.peek());
+         * System.out.println(adq.peekFirst());
+         * System.out.println(adq.peekLast());
+         * 
+         * System.out.println(adq.poll());
+         * System.out.println("poll() :" + adq);
+         * 
+         * System.out.println(adq.pollFirst());
+         * System.out.println("poll() :" + adq);
+         * 
+         * System.out.println(adq.pollLast());
+         * System.out.println("poll() :" + adq);
          * 
          * System.out.println(adq.isEmpty());
          */
