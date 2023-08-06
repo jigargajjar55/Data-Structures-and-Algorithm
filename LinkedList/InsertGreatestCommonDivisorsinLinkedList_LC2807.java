d758756e03e50087eb0572ea565079e359d2e9d0
Jigar Gajjar <jigargajjar.55jg@gmail.com>
2023-08-06 14:22:55 +0530

    45th Commit

---
+++ LinkedList/InsertGreatestCommonDivisorsinLinkedList_LC2807.java
@@ -0,0 +1,54 @@
+package LinkedList;
+import java.util.*;
+
+
+class Solution {
+    
+    class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+
+    private int getGCD(int a,int b){        
+        
+        if(b == 0){
+            return a;
+        }
+        
+        return getGCD(b, a % b);
+        
+    }
+    
+    public ListNode insertGreatestCommonDivisors(ListNode head) {
+        
+        ListNode curr = head;
+        int val1 = curr.val;
+        
+        ListNode prev = curr;        
+        curr = curr.next;
+        
+        while(curr != null){
+            
+            int val2 = curr.val;
+            
+            int gcdValue = getGCD(val1,val2);
+            
+            ListNode newNode = new ListNode(gcdValue);
+            prev.next = newNode;
+            prev = prev.next;
+            prev.next = curr;
+            
+            prev = curr;
+            curr = curr.next;
+            
+            val1 = val2;
+
+        }
+       
+        return head;
+        
+    }
+}
